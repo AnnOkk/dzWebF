d3313ec5375175fca1c2de4f6c9467dce55f1ea6
AnnOkk <chereshnyasladkaya@gmail.com>
2025-09-08 15:23:58 +0300

    Update dzWeb.js

--- dz-webF/dzWeb.js
+++ dz-webF/dzWeb.js
@@ -2,6 +2,36 @@ let arr = [123, 345, 'assdf', true, '678'];
 let res = sumOddDigits(arr);
 console.log(res);
 
+res = sumOddDigits2(arr);
+console.log(res);
+
+
+function sumOddDigits2(arr) {
+    let arr3 = [];
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let n = arr[i];
+
+        if(typeof n === 'boolean') continue;
+
+        let n2=Number(n);
+
+        if(Number.isFinite(n2)){
+            if(n2<0) n2=-n2;
+            let sum = 0;
+            while(n2!==0){
+                let dg = n2%10;
+                if(dg%2!==0) sum +=dg;
+                n2=Math.floor(n2/10);
+            }
+            arr3.push(sum);
+        }
+    }
+
+    return arr3;
+}
+
+ 
 function sumOddDigits(arr) {
     let len = arr.length;
     let arr2 = [];
@@ -9,7 +39,7 @@ function sumOddDigits(arr) {
     for (let i = 0; i < len; i++) {
         let num = +(arr[i]);
 
-        if (!isNaN(num) && typeof arr[i] !=='boolean') {
+        if (!isNaN(num) && typeof arr[i] !=='boolean' && num != Infinity && num != -Infinity) {
             if (num < 0) num = -num;
             let sum = 0;
 
@@ -26,5 +56,6 @@ function sumOddDigits(arr) {
     }
     return arr2;
 }
+
 
 
